ba2540f0ef36e672f4f2a1c05b12261c05db1aad
Luis Alvarez <lalvarezguillen@gmail.com>
2020-02-02 20:13:07 +0000

    manually encoding form

--- script/contactForm.js
+++ script/contactForm.js
@@ -1,6 +1,26 @@
 function submitContactForm(ev, form) {
     ev.preventDefault();
-    fetch(form.action,{method:'post', body: new FormData(form)})
+    const data = new FormData(form)
+    let urlEncoded = `name=${data.get('name')}`;
+    urlEncoded += `&email=${data.get('email')}`;
+    urlEncoded += `&subject=${data.get('subject')}`;
+    urlEncoded += `&content=${data.get('content')}`;
+    if (data.get('phone')) {
+        urlEncoded += `&phone=${data.get('phone')}`;
+    }
+    if (data.get('company_name')) {
+        urlEncoded += `&company_name=${data.get('company_name')}`;
+    }
+    urlEncoded = encodeURI(urlEncoded);
+    fetch(form.action,
+        {
+            method:'post',
+            body: urlEncoded,
+            headers: new Headers({
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }),
+        }
+    )
         .then(() => {
             // ev.preventDefault();
             form.reset();
